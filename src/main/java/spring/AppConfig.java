b9e6e3213ef7915e0bc1eb9dca21724f43f0aebb
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-04-18 13:59:44 +0200

    konfiguracja

---
+++ src/main/java/spring/AppConfig.java
@@ -0,0 +1,14 @@
+package spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("spring")
+public class AppConfig implements WebMvcConfigurer {
+}
+
+
